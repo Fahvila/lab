cdf00abb2aa4e118bb4ddd02283ce3ac613dd657
Fahvila <vilasinee_putsiri@gmail.ac.th>
2024-01-30 15:41:54 +0700

    lab15

--- lab15_5.cpp
+++ lab15_5.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &,int &,int &);
+void shuffle(int *, int *, int *, int *);
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,24 +11,22 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d);
+	    shuffle(&a, &b, &c, &d);
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
 	return 0;
 }
 
-void shuffle(int &a, int &b, int &c, int &d) {
-    int arr[] = {a, b, c, d};
-    int n = sizeof(arr) / sizeof(arr[0]);
+void shuffle(int *a, int *b, int *c, int *d) {
+    int temp;
 
-    for (int i = n - 1; i > 0; i--) {
+    int *arr[] = {a, b, c, d};
+
+    for (int i = 3; i > 0; i--) {
         int j = rand() % (i + 1);
-        swap(arr[i], arr[j]);
+        temp = *arr[i];
+        *arr[i] = *arr[j];
+        *arr[j] = temp;
     }
-
-    a = arr[0];
-    b = arr[1];
-    c = arr[2];
-    d = arr[3];
 }
